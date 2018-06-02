6358b8c8a2447686e4b1db2a72e6a5bee2ad6892
Damandeep Singh <mail@damandeepsingh.com>
2018-06-03 01:10:06 +0530

    Added readme

---
+++ inject/inject.js
@@ -0,0 +1,96 @@
+
+var url = chrome.extension.getURL ("src/page_action/page_action.html");
+var lbl = "";
+
+console.log(url)
+var stylePrefix = new URL(url).hostname;
+chrome.storage.sync.get('data', function(data) {
+    lbl = data.data;
+    if(document.body.getAttribute("qr-extension-ready")!="yes")
+    {
+
+        /** Adding Head scripts */
+        const scripts = ['/js/qr.js','/js/qr-code.js'];
+        for(let scr of scripts) {
+            const scrUrl = chrome.extension.getURL (scr);
+            const scrElem = document.createElement("script");
+            scrElem.src = scrUrl;
+            console.log(scrUrl)
+            document.head.append(scrElem);
+        }
+
+        /** Adding custom style node */
+        const styleContent = `
+        .` + stylePrefix + `_wrapper {
+            position: fixed;
+            text-align: center;
+            top: 50%;
+            left: 50%;
+            display: inline-block;
+            z-index: 1199;
+            min-width: 200px;
+            transform: translate(-50%, -50%);
+            min-height: 100px;
+            background: #fff;
+            padding: 30px;
+            border: 1px solid #3f00ff;
+            box-shadow: 0 0 21px -5px #1e1e1e;
+        }
+        #` + stylePrefix + `_label {
+            font-family: Segoe UI, Lucida Grande, Arial, Verdana;
+            font-size: 14px;
+            display: block;
+            word-wrap: break-word;
+            word-break: break-all;
+        }
+        #` + stylePrefix + `_close {
+            position: absolute;
+            right: -1px;
+            text-decoration: none;
+            font-family: Segoe UI, Lucida Grande, Arial, Verdana;
+            top: -1px;
+            height: 30px;
+            width: 30px;
+            line-height: 30px;
+            background: #3f00ff;
+            text-align: center;
+            color: #fff;
+            font-size: 18px;
+            border-radius: 1px;
+        }
+
+        #` + stylePrefix + `_qrcode {
+            position: relative;
+            font-family: Segoe UI, Lucida Grande, Arial, Verdana;
+            z-index: 1200;
+        }
+        `
+        const styleElem = document.createElement('style');
+        styleElem.innerHTML = styleContent;
+        document.head.appendChild(styleElem);
+
+        /** add structure */
+        const wrapper  = document.createElement("div");
+        wrapper.classList.add(stylePrefix + '_wrapper');
+        const wrapperContent = `
+            <a id="` + stylePrefix +`_close" href="#">X</a>
+            <qr-code data="` + lbl + `" id="` + stylePrefix +`_qrcode"></qr-code>
+            <label id="` + stylePrefix +`_label">` + lbl +`</label>`;
+        wrapper.innerHTML = wrapperContent;
+        document.body.insertBefore (wrapper, document.body.firstChild);
+
+
+        /** Add event listeners */
+        document.getElementById( stylePrefix + '_close').addEventListener('click', e => {
+            document.querySelector('.' + stylePrefix +  '_wrapper').style.display = "none";
+            e.preventDefault();
+            e.stopPropagation();
+        })
+        document.body.setAttribute('qr-extension-ready', "yes")
+    } else {
+        document.querySelector('#' + stylePrefix +  '_qrcode').setAttribute("data", lbl);
+        document.querySelector('#' + stylePrefix +  '_label').innerHTML = lbl;
+        document.querySelector('.' + stylePrefix +  '_wrapper').style.display = "inline-block";
+    }
+    
+});
---
+++ readme.md
@@ -0,0 +1,8 @@
+# QRify
+This google chrome extension enable user to generate QR code for any selected text/links, or page URL. It makes it easy to share the same info to other devices with QR reader. Passing a link/info to your mobile is very easy, Just select the text/link, right click and select QRify. It will generate the QR code on the screen. Use any QR reader app to open the info on your device.
+
+## Technologies Used
+QRcode webcomponent - https://github.com/educastellano/qr-code
+
+## Limitations
+Currently maximum character are capped at 500, as most mobile QR scanner really struggle to read complex QR from lower PPI screen.
